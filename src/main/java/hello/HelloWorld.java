710022d457c93d67f6ced0aca0dc3dbd952238af
Imani Pelton <imani.pelton@canonical.com>
2025-07-25 10:15:44 -0400

    docs: add maven java example

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.starcraft.hello;
+
+import io.github.backpaper0.example.hello.Hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        System.out.println(hello.greeting());
+        return;
+    }
+}
